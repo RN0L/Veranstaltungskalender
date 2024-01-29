b1e7facd019d16e613aa9f871e53914921322075
RN0L <leonardharnold@gmail.com>
2024-01-29 12:52:53 +0100

    löschen in app.py

--- app.py
+++ app.py
@@ -5,18 +5,6 @@ app = Flask(__name__)
 
 bootstrap = Bootstrap(app) 
 
-Einkaufsliste = [  
-
-    {'name': 'Ochse', 'anzahl':'2'},
-
-    {'name': 'Birne', 'anzahl':'2'},
-
-    {'name': 'Banane', 'anzahl':'2'},
-
-    {'name': 'Hackfleisch', 'anzahl':'2'},
-
-    {'name': 'Kackfleisch', 'anzahl':'2'}
-] 
 
 app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'lux'
 
@@ -24,17 +12,7 @@ app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'lux'
 def einkaufsliste():
     return render_template('index.html', einkaufsliste = Einkaufsliste)
 
-@app.route('/laender')
-def laender():
-    return render_template('staftlandfluss.html', einkaufsliste = Einkaufsliste)
 
-@app.route('/form')
-def form():
-    return render_template('form.html')
-
-@app.route('/v-bucks') 
-def login(): 
-    return render_template('login.html') 
 
 if __name__ == '__main__':  
 
